b0887468526be810863ddfca8cc0ec6f8a9b7a4d
Urszula Drabińska <u.drabinska@gmail.com>
2018-03-02 20:25:11 +0100

    Fix: 10 elements instead of 9

--- app/src/main/java/com/example/android/miwok/NumbersActivity.java
+++ app/src/main/java/com/example/android/miwok/NumbersActivity.java
@@ -44,7 +44,7 @@ public class NumbersActivity extends AppCompatActivity {
         LinearLayout rootView = findViewById(R.id.rootView);
 
         int index = 0;
-        while (index < 9) {
+        while (index < 10) {
             TextView wordView = new TextView(this);
             wordView.setText(words.get(index));
             rootView.addView(wordView);
