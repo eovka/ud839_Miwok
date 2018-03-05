90e4cfcaaf4d31e80c096d48ee6e7941c1f6a144
Urszula Drabińska <u.drabinska@gmail.com>
2018-03-05 19:11:45 +0100

    Not needed context part in Word class deleted

--- app/src/main/java/com/example/android/miwok/Word.java
+++ app/src/main/java/com/example/android/miwok/Word.java
@@ -1,27 +1,20 @@
 package com.example.android.miwok;
 
-import android.content.Context;
-
 public class Word {
+    // class state
     private String mMiwokTranslation;
     private String mDefaultTranslation;
-    private Context mContext;
 
-    public Word(Context context) {
-        mMiwokTranslation = "";
-        mDefaultTranslation = "";
-        mContext = context;
-    }
-
+    // class constructor
     public Word(String miwokTranslation, String defaultTranslation) {
         mMiwokTranslation = miwokTranslation;
         mDefaultTranslation = defaultTranslation;
     }
 
+    // class methods
     public String getMiwokTranslation() {
         return mMiwokTranslation;
     }
-
     public String getDefaultTranslation() {
         return mDefaultTranslation;
     }
