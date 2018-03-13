e34543253709adb6d8d6bf3e847daa77a796bb08
Urszula Drabińska <u.drabinska@gmail.com>
2018-03-13 23:38:10 +0100

    Family and phrases activities added

--- app/src/main/java/com/example/android/miwok/FamilyActivity.java
+++ app/src/main/java/com/example/android/miwok/FamilyActivity.java
@@ -17,6 +17,9 @@ package com.example.android.miwok;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class FamilyActivity extends AppCompatActivity {
 
@@ -24,5 +27,22 @@ public class FamilyActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_family);
+
+        ArrayList<Word> words = new ArrayList<>();
+        words.add(new Word("әpә", "father"));
+        words.add(new Word("әṭa", "mother"));
+        words.add(new Word("angsi", "son"));
+        words.add(new Word("tune", "daughter"));
+        words.add(new Word("taachi", "older brother"));
+        words.add(new Word("chalitti", "younger brother"));
+        words.add(new Word("teṭe", "older sister"));
+        words.add(new Word("kolliti", "younger sister"));
+        words.add(new Word("ama", "grandmother"));
+        words.add(new Word("paapa", "grandfather"));
+
+        WordAdapter wordAdapter = new WordAdapter(this, words);
+        ListView listView = findViewById(R.id.family_list);
+        listView.setAdapter(wordAdapter);
+
     }
 }
