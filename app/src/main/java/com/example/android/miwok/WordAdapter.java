b282e91ff4e8ef901b384cd2fe077f15fd7cb7eb
Urszula Drabińska <u.drabinska@gmail.com>
2018-03-25 13:12:04 +0200

    ImageView added to WordAdapter

--- app/src/main/java/com/example/android/miwok/WordAdapter.java
+++ app/src/main/java/com/example/android/miwok/WordAdapter.java
@@ -7,6 +7,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import java.util.ArrayList;
@@ -33,6 +34,8 @@ public class WordAdapter extends ArrayAdapter<Word> {
         miwokTranslationView.setText(currentWord.getMiwokTranslation());
         TextView defaultTranslationView = listItemView.findViewById(R.id.default_word);
         defaultTranslationView.setText(currentWord.getDefaultTranslation());
+        ImageView wordImage = listItemView.findViewById(R.id.word_icon);
+        wordImage.setImageResource(currentWord.getImageResourceId());
         return listItemView;
     }
 }
